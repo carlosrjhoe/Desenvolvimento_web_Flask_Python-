b2b0687152e1b491fa954c44cd6a1ed5474fed8b
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-07-14 10:55:41 -0300

    Flsk: Criando uma nova rota, e adcionando o method POST para cadastrar e renderizar a requizição

--- JogoTeca/jogoteca.py
+++ JogoTeca/jogoteca.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
+
+# Variáveis Global
+jogo_01 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo_02 = Jogo('Skyrum', 'RPG', 'PC')
+jogo_03 = Jogo('Crash', 'Ação', 'Playstation')
+jogo_04 = Jogo('Final Fantasy VII', 'RPG', 'Playstation')
+lista_de_jogos = [jogo_01, jogo_02, jogo_03, jogo_04]
         
 app = Flask(__name__)
 
 # Criando a rota principal
 @app.route('/')
-def inicio():
-    jogo_01 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo_02 = Jogo('Skyrum', 'RPG', 'PC')
-    jogo_03 = Jogo('Crash', 'Ação', 'Playstation')
-    jogo_04 = Jogo('Final Fantasy VII', 'RPG', 'Playstation')
-    lista_de_jogos = (jogo_01, jogo_02, jogo_03, jogo_04)
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista_de_jogos)
 
 # Criando uma nova rota
@@ -23,5 +25,14 @@ def inicio():
 def cadastrar():
     return render_template('cadastrar.html', titulo='Cadastrar novo jogo')
 
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista_de_jogos.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista_de_jogos)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
